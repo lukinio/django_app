707a049e7a36a2187a961449034a3614d27481d5
Łukasz Pustelnik <lukias881@gmail.com>
2019-12-02 20:44:40 +0100

    create NoteDetailView

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from notes.views import NoteListView
+from notes.views import NoteListView, NoteDetailView
 
 urlpatterns = [
     url(r'^$', NoteListView.as_view(), name='note-list'),
+    url(r'^(?P<pk>\d+)/$', NoteDetailView.as_view(), name='note-detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from notes.models import Note
 
 
 class NoteListView(ListView):
+    model = Note
+
+class NoteDetailView(DetailView):
     model = Note
